25870cdf8cf198d75adf63b47723048a3406bfd0
Christian Meda <cmargueiz@HP-EliteBook-8460p-aaa1e686.localdomain>
2021-10-03 15:50:38 -0600

    Inicializacion de jquery

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+       
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+  $('.dropdown-trigger').dropdown();
