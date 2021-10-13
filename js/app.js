ae6d81c730b776eb0555d98c984a93a8415f9721
Guillex <mf17006@ues.edu.sv>
2021-10-12 21:21:43 -0600

    Mejoras en la app

--- js/app.js
+++ js/app.js
@@ -2,13 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+ 
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
 
   // Or with jQuery
 
   $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+    $('select').formSelect();
+  });     
