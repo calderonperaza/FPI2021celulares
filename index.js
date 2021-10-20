cae3a5f1fdfb5c96b21377f0505bc3ff038424ab
neto0601 <ernestoalonso1998@gmail.com>
2021-10-20 15:45:59 -0600

    terminando las cards paraa desktop y movil

---
+++ index.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let cardsTelefonos = document.getElementById('cardsTelefonos');
+
+    let partes = '';
+
+    for(let i = 0; i < 8; i++){
+    partes+=`
+        <div class="col-3 p-2 d-none d-lg-block d-md-block">
+            <div class="card" >
+                <img src="./imagenes/iphone12pro.jpg" class="card-img-top" alt="..." style="height: 200px;">
+                <div class="card-body text-center">
+                    <h5 class="card-title">$1102.00</h5>
+                    <p class="card-text">
+                    Iphone 12 Pro Max
+                    Super Retina XDR
+                    OLED de 6,7"
+                    128GB
+                    FullHD+ a 2.778 x 1.184
+                    12 Megapixeles
+                    458 ppp
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 p-2 d-md-none">
+            <div class="card" >
+                <img src="./imagenes/iphone12pro.jpg" class="card-img-top" alt="..." style="height: 200px;">
+                <div class="card-body text-center">
+                    <h5 class="card-title">$1102.00</h5>
+                    <p class="card-text">Iphone 12 Pro Max
+                    Super Retina XDR
+                    OLED de 6,7"
+                    128GB
+                    FullHD+ a 2.778 x 1.184
+                    12 Megapixeles
+                    458 ppp
+                    </p>
+                </div>
+            </div>
+        </div>
+            `
+}
+cardsTelefonos.innerHTML = partes;
+})
