e3d10b9ddf863e9e120fd740415073025603f3fc
Kelvin Rojas <rc19023@ues.edu.sv>
2021-10-24 01:01:59 -0600

    website responsive: index, articulo, nuevo

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
